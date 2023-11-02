088375eda07d0eb368db73f558b1a5cd2410ad02
Anamika-s <anamika.sawhney22@gmail.com>
2023-11-02 12:52:32 +0530

    added loop

--- CodeFirstApproach/Program.cs
+++ CodeFirstApproach/Program.cs
@@ -8,91 +8,97 @@ namespace CodeFirstApproach
         static StudentDbContext db = new StudentDbContext();
         static void Main(string[] args)
         {
-            int c;
-            Console.WriteLine("Enter choice");
-            c = byte.Parse(Console.ReadLine());
-            switch (c)
+            string choice = "y";
+            while (choice == "y")
             {
-                case 1:
-                    {
-                        Console.WriteLine("Enter Name");
-                        string name = Console.ReadLine();
-                        Console.WriteLine("Enter Batch");
-                        string batch = Console.ReadLine();
-                        Console.WriteLine("Enter Marks");
-                        int marks = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Enter Address");
-                        string address = Console.ReadLine();
-                        Student student = new Student()
+                int c;
+                Console.WriteLine("Enter choice");
+                c = byte.Parse(Console.ReadLine());
+                switch (c)
+                {
+                    case 1:
                         {
-                            Name = name,
-                            Address = address,
-                            Batch = batch,
-                            Marks = marks
-                        };
-                        InsertRecord(student);
-
-                        break;
-                    }
-                case 2:
-                    {
-                        List<Student> list = GetStudents();
-                        if (list == null)
-                            Console.WriteLine("There are no records");
-                        else
-                            foreach (Student student in list)
+                            Console.WriteLine("Enter Name");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Enter Batch");
+                            string batch = Console.ReadLine();
+                            Console.WriteLine("Enter Marks");
+                            int marks = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Address");
+                            string address = Console.ReadLine();
+                            Student student = new Student()
                             {
-                                Console.WriteLine(student.Name);
-                            }
-                        break;
-                    }
-                case 3:
-                    {
-                        Console.WriteLine("Enter name to search");
-                        string name = Console.ReadLine();
-                        Student student = SearchRecord(name);
-                        if (student == null)
-                            Console.WriteLine("No record with this name");
-                        else
-                            Console.WriteLine(student.Name + " " + student.Address);
-                        break;
-                    }
-                case 4:
-                    {
-                        Console.WriteLine("Enter id to delete");
-                        int id = Byte.Parse(Console.ReadLine());
-                        bool flag = DeleteRecord(id);
-                        if (flag == false)
-                            Console.WriteLine("No record with this id");
-                        else
-                            Console.WriteLine("DeletedRecord ");
-                        break;
-                    }
-                case 5:
-                    {
-                        Console.WriteLine("Enter id to edit");
-                        int id = Byte.Parse(Console.ReadLine());
-                        Console.WriteLine("Enter new Batch");
-                        string batch = Console.ReadLine();
-                        Console.WriteLine("Enter new Marks");
-                        int marks = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Enter new Address");
-                        string address = Console.ReadLine();
-                        Student temp = new Student()
-                        {
-                             
-                            Address = address,
-                            Batch = batch,
-                            Marks = marks
-                        };
-                        bool flag = EditRecord(id, temp);
-                        if (flag == false)
-                            Console.WriteLine("No record with this id");
-                        else
-                            Console.WriteLine("DeletedRecord ");
-                        break;
-                    }
+                                Name = name,
+                                Address = address,
+                                Batch = batch,
+                                Marks = marks
+                            };
+                            InsertRecord(student);
 
+                            break;
+                        }
+                    case 2:
+                        {
+                            List<Student> list = GetStudents();
+                            if (list == null)
+                                Console.WriteLine("There are no records");
+                            else
+                                foreach (Student student in list)
+                                {
+                                    Console.WriteLine(student.Name);
+                                }
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Enter name to search");
+                            string name = Console.ReadLine();
+                            Student student = SearchRecord(name);
+                            if (student == null)
+                                Console.WriteLine("No record with this name");
+                            else
+                                Console.WriteLine(student.Name + " " + student.Address);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter id to delete");
+                            int id = Byte.Parse(Console.ReadLine());
+                            bool flag = DeleteRecord(id);
+                            if (flag == false)
+                                Console.WriteLine("No record with this id");
+                            else
+                                Console.WriteLine("DeletedRecord ");
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Enter id to edit");
+                            int id = Byte.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter new Batch");
+                            string batch = Console.ReadLine();
+                            Console.WriteLine("Enter new Marks");
+                            int marks = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter new Address");
+                            string address = Console.ReadLine();
+                            Student temp = new Student()
+                            {
+
+                                Address = address,
+                                Batch = batch,
+                                Marks = marks
+                            };
+                            bool flag = EditRecord(id, temp);
+                            if (flag == false)
+                                Console.WriteLine("No record with this id");
+                            else
+                                Console.WriteLine("DeletedRecord ");
+                            break;
+                        }
+
+                }
+                Console.WriteLine("Do you want to repeat");
+                choice = Console.ReadLine();
             }
         }
 
